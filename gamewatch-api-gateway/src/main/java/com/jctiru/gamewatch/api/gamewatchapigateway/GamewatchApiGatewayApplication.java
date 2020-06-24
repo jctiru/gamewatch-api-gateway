94126f5be6dcfe7e66d732b01fee2f35eeb7ac5c
jctiru <jonathanchristiantiru@gmail.com>
2020-06-24 23:03:41 +0800

    Initial commit

---
+++ gamewatch-api-gateway/src/main/java/com/jctiru/gamewatch/api/gamewatchapigateway/GamewatchApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jctiru.gamewatch.api.gamewatchapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamewatchApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamewatchApiGatewayApplication.class, args);
+	}
+
+}
